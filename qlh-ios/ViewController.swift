f51298f29226a7d82f222756083dfe25ac419bf5
Daniel Avram <dani.avram@ymail.com>
2019-04-13 17:37:33 +0300

    Initial Commit

---
+++ qlh-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  qlh-ios
+//
+//  Created by Daniel Avram on 13/04/2019.
+//  Copyright © 2019 Daniel Avram. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
